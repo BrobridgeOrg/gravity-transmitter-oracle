11458123b55e9217c2f1a323ef988d782696b105
jhe <jhe@brobridge.com>
2021-05-28 18:06:33 +0800

    upgrade gravity-api and gravity-sdk to support new subscriber

--- pkg/app/types.go
+++ pkg/app/types.go
@@ -2,12 +2,8 @@ package app
 
 import (
 	"github.com/BrobridgeOrg/gravity-transmitter-oracle/pkg/database"
-	"github.com/BrobridgeOrg/gravity-transmitter-oracle/pkg/grpc_server"
-	"github.com/BrobridgeOrg/gravity-transmitter-oracle/pkg/mux_manager"
 )
 
 type App interface {
-	GetGRPCServer() grpc_server.Server
-	GetMuxManager() mux_manager.Manager
 	GetWriter() database.Writer
 }
